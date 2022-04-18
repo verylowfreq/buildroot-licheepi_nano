6b3b2754f5c1c5a98335813f227864624d1f3b73
verylowfreq <60875431+verylowfreq@users.noreply.github.com>
2022-04-19 00:18:13 +0900

    Update kernel bootargs.

--- board/licheepi_nano/boot.cmd
+++ board/licheepi_nano/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=tty0 console=ttyS0,115200 panic=5 rootwait root=/dev/mmcblk0p2 ro
+setenv bootargs console=tty0 console=ttyS0,115200 panic=5 rootwait root=/dev/mmcblk0p2 ro fbcon=logo-count:0
 load mmc 0:1 0x80C00000 suniv-f1c100s-licheepi-nano-custom.dtb
 load mmc 0:1 0x80008000 zImage
 bootz 0x80008000 - 0x80C00000
